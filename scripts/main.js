aa1eaf204dbbd9e8fdb098bcfc28efe5c835fa8b
Erwin Ibarra <weito@live.com>
2020-06-09 19:44:27 -0500

    2020
    2020

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+
+$(document).ready(function() {
+  AOS.init( {
+    // uncomment below for on-scroll animations to played only once
+    // once: true  
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$('a.smooth-scroll')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+
+// Send Email
+
+function sendEmail() {
+  var input = document.getElementById('subject');
+  subject = input.value;
+  var input = document.getElementById('body');
+  body = input.value;
+
+  window.location.href = "mailto:Iam@ErwinIbarraV.com?cc=Erwin.Ibarra@Outlook.com"+ "&subject=" + subject + "&body=" + body;
+}
